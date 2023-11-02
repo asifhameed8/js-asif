b3d40ead423ca37a1889121ac794397a5082fd88
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-02 13:05:56 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert("hello"); // we are using nodejs not browser
+
+let name = "Abdul Hameed"
+let age = 50
+let isLoggedIn = true
+
+// number 
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Asif")
+console.log( typeof undefined) // undefined
+console.log( typeof null) // object
