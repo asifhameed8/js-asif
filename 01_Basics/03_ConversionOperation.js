61e8eb5306d2962993ccc905c0181d049a266b8e
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-03 12:42:54 +0000

    conversions

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,9 +1,17 @@
 let score = undefined
 
-console.log(typeof  score);
-console.log(typeof(score))
+//console.log(typeof  score);
+//console.log(typeof(score))
 
 let valueInNumber = Number(score);
 
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)
+//console.log(typeof(valueInNumber))
+//console.log(valueInNumber)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
