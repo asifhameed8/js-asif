b8c26aea2e7eb43b5f250f7ed1857e3a45ed069e
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-04 13:14:49 +0000

    operations

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -14,4 +14,24 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+let value = 3
+let negativeValue = -value;
+//console.log(negativeValue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/3)
+console.log(2%3)
+console.log(2**3)
+
+let str1 = "hello"
+let str2 = " asif"
+
+let str3 = str1 + str2;
+//console.log(str3);
+
+console.log ("1" + 2)
+console.log (1 + "2")
