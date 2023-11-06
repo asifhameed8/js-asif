27250c8dc90339e78da0dc38b3bcaa790815af9a
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-06 09:57:32 +0000

    operations

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -20,12 +20,12 @@ let value = 3
 let negativeValue = -value;
 //console.log(negativeValue);
 
-console.log(2+2)
-console.log(2-2)
-console.log(2*2)
-console.log(2/3)
-console.log(2%3)
-console.log(2**3)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/3)
+// console.log(2%3)
+// console.log(2**3)
 
 let str1 = "hello"
 let str2 = " asif"
@@ -33,5 +33,16 @@ let str2 = " asif"
 let str3 = str1 + str2;
 //console.log(str3);
 
-console.log ("1" + 2)
-console.log (1 + "2")
+// console.log ("1" + 2)
+// console.log (1 + "2")
+// console.log ("1" + 2 + 2)
+// console.log (1 + 2 + "2")
+// console.log (3+4*5%2)
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2;
