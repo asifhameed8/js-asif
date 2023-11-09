868b9f238c48d8697585e32eb084baa866fc9811
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-09 12:09:58 +0000

    data types summary

--- 01_Basics/datatypes-sumary.js
+++ 01_Basics/datatypes-sumary.js
@@ -1,5 +1,19 @@
 // primitive value type
 // 7 Types: String Number Boolean null undefined symbol BigInt
 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 34444533355332444n
+
 // non primitive/ Reference type
 // Array, Object, function
