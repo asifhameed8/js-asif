e7bb3c5202599bd51e1d6c02237e84c2f10036fe
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-16 15:32:01 +0000

    string changes

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,14 @@
+const gameName = new String("Asif-Hameed");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('f'));
+
+var newString = gameName.substring(0,4);
+// console.log(newString);
+
+var anotherString = gameName.slice(-8,1);
+console.log(anotherString);
