a2cb2c1e3f7eecfa0c600636af2dae4cb8307037
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-20 12:45:39 +0000

    maths

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -1,4 +1,15 @@
 const score = 100
-console.log(score)
+// console.log(score)
 const balance = new Number(400)
-console.log(balance);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1218.500
+// console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-PK'))
+
+
+// ++++++++++++++++ Maths ++++++++++++
