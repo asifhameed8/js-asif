16726201f86063a5b01024b772c37b6e061d8810
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-21 10:29:16 +0000

    maths

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -13,3 +13,8 @@ const hundreds = 1000000
 
 
 // ++++++++++++++++ Maths ++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
