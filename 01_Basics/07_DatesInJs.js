41fc5ef9c768c74a6121d57bfce844972bf913c4
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-22 13:50:24 +0000

    Dates

---
+++ 01_Basics/07_DatesInJs.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+let myCreatedDate = new Date(2023,4,3,5,3)
+console.log(myCreatedDate.toLocaleString())
