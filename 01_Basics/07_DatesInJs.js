89f9657fe0b73b89504e4ec048160d2d928d7270
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-11-23 12:54:00 +0000

    date video

--- 01_Basics/07_DatesInJs.js
+++ 01_Basics/07_DatesInJs.js
@@ -7,4 +7,24 @@ let myDate = new Date()
 // console.log(myDate.toLocaleTimeString());
 // console.log(typeof myDate);
 let myCreatedDate = new Date(2023,4,3,5,3)
-console.log(myCreatedDate.toLocaleString())
+// console.log(myCreatedDate.toLocaleString())
+
+myCreatedDate = new Date("01/14/2023")
+
+
+ console.log(myCreatedDate.toLocaleString())
+
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date();
+console.log(newDate.getMonth()+1)
+console.log(` Today the date is ${newDate.getDay()} and month is ${newDate.getMonth()+1}`)
+
+newDate.toLocaleString('default', {
+ weekday: "long"
+
+})
