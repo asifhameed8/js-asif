a6e48140fb66623e200a160a41ddeb732d61ac5f
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-12-11 14:40:17 +0000

    array of

--- 02_Basics/02_Arrays.js
+++ 02_Basics/02_Arrays.js
@@ -12,6 +12,16 @@ console.log(allHeros)
 const all_new_heros = [...marvel_heros, ...dc_heros]
 console.log(all_new_heros)
 
-const another_array = [1,2,3, [4,5,6],[6,7,[4,5]]]
+const another_array = [1,2,3, [4,5,6],7,[6,7,[4,5]]]
 
-const real_another_array = []
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array)
+
+console.log(Array.isArray("AsifHameed"))
+console.log(Array.from("AsifHameed"))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
