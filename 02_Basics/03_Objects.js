af4869ccdce4c296a38e3de316788b52eb74fc3b
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-12-13 15:48:06 +0000

    objects

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,23 @@
+// Singleton
+// Object.create
+
+
+// Object literal
+
+const mySym = new Symbol("key1")
+
+const jsUser = {
+    name: "asif",
+    "full name": "asif hameed",
+    age: 22,
+    location: "Lahore",
+    email: "asif@gmail.com",
+    isLoggedIn: false,
+    LastLoginDays: ["Mnday", "Saturday"]
+}
+console.log(jsUser["full name"])
+console.log(jsUser["email"])
+
+
+myArray = ["a", "b"]
+// console.log(myArray[1])
