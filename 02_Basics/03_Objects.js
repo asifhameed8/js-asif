d3179e8bb66f594eaaa744e97ba9503251dbf1a3
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2023-12-21 11:35:49 +0000

    objects

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -4,20 +4,39 @@
 
 // Object literal
 
-const mySym = new Symbol("key1")
+const mySym = Symbol("key1")
 
 const jsUser = {
     name: "asif",
     "full name": "asif hameed",
+    [mySym]: "mykey1",
     age: 22,
     location: "Lahore",
     email: "asif@gmail.com",
     isLoggedIn: false,
     LastLoginDays: ["Mnday", "Saturday"]
 }
-console.log(jsUser["full name"])
-console.log(jsUser["email"])
 
+// console.log(jsUser["full name"])
+// console.log(jsUser["email"])
+// console.log(jsUser[mySym])
+
+jsUser.email = "asif@cc.com"
+// Object.freeze(jsUser)
+jsUser.email = "asif@tt.com"
+
+// console.log(jsUser)
+
+jsUser.greeting =  function(){
+    console.log("Hello JS User")
+}
+
+jsUser.greetingTwo =  function(){
+    console.log(`Hello JS User, ${this.email}`)
+}
+
+console.log(jsUser.greeting())
+console.log(jsUser.greetingTwo())
 
 myArray = ["a", "b"]
 // console.log(myArray[1])
