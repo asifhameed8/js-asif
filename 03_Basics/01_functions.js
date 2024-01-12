9f9542dcd2fc476fa63001411d479cdb1cda5d3d
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2024-01-12 12:05:42 +0000

    functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -49,3 +49,17 @@ console.log(`username is ${anyObject.username} and price is ${anyObject.price}`)
 }
 
 handleObject(user)
+
+handleObject({
+  username: "Kashif",
+  price: 29
+})
+
+const myNewArray = [200,400,600,100]
+
+function returnSecondValue(getArray){
+ return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([100,300,200,400]))
