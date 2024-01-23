2f77a081514b16672d66d2441476b2a0a4ad7287
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2024-01-23 10:54:17 +0000

    Arrow functions.

--- 03_Basics/03_Arrow.js
+++ 03_Basics/03_Arrow.js
@@ -13,9 +13,37 @@ welcomeMessage: function(){
 
 //console.log(this)
 
-function chai(){
-    let username = "asif"
-    // console.log(this)
-    console.log(this.username)
+// function chai(){
+//     let username = "asif"
+//     // console.log(this)
+//     console.log(this.username)
+// }
+//chai()
+
+// const chai = function(){
+//   let username = "asif"
+//   console.log(this.username)
+// }
+
+const chai = () => {
+  let username = "asif"
+  console.log(this)
 }
 chai()
+
+// const addTwo = (num1, num2) => {
+//   return num1 + num2;
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2;
+
+// const addTwo = (num1, num2) =>  (num1 + num2);
+
+const addTwo = (num1, num2) =>  ({username : "asif"});
+
+const myArr = [1,2,3,4,5]
+
+myArr.array.forEach(() => ());
+
+
+console.log(addTwo(3,4))
