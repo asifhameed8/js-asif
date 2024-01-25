3b58e4f0416ba62848ac49ef545df66af74e770e
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2024-01-25 13:17:33 +0000

    named IIFE

--- 03_Basics/04_IIFE.js
+++ 03_Basics/04_IIFE.js
@@ -1,5 +1,14 @@
 // immediately invoked function expressions
 
 (function chai(){
+    // named IIFE
     console.log('Function executed')
-})()
+})();
+
+(() => {
+    console.log('Function executed two')
+})();
+
+((name) => {
+    console.log(`Function executed two ${name}`)
+})('Asif')
