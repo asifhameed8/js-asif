3e29d72bc747646be653e8701f08c99db9538d34
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2024-01-31 10:37:43 +0000

    conditional

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,13 @@
+// if
+const temperature = 41
+// if(2 == "2"){
+    // if(2 != 3){
+    if(temperature === 41){
+     console.log("less than 50")
+}else{
+    console.log("temperature is greater than 50")
+}
+
+console.log("executed")
+
+// < , > , <= , >=, ==, !=, ===, !==
