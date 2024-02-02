a9bb2f88bab4442a51a1d07e43ff37a6616c9c7c
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2024-02-02 12:38:11 +0000

    conditional if else

--- 04_ControlFlow/one.js
+++ 04_ControlFlow/one.js
@@ -2,12 +2,33 @@
 const temperature = 41
 // if(2 == "2"){
     // if(2 != 3){
-    if(temperature === 41){
-     console.log("less than 50")
-}else{
-    console.log("temperature is greater than 50")
-}
+//     if(temperature === 40){
+//      console.log("less than 50")
+// }else{
+//     console.log("temperature is greater than 50")
+// }
 
-console.log("executed")
+// console.log("executed")
 
 // < , > , <= , >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`)
+// }
+
+// console.log(`user power: ${power}`)
+
+const balance = 200
+
+// if(balance > 200) console.log("test"),
+// console.log("test2")
+
+if(balance < 500){
+    console.log("Less than 500")
+}
+else if(balance < 750){
+    console.log("Less than 750")
+}
