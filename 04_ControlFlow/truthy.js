bf99f17d3a45476b55056e0be43dace47b14bf47
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2024-02-09 10:49:34 +0000

    Nullish coalescing operator(??)

--- 04_ControlFlow/truthy.js
+++ 04_ControlFlow/truthy.js
@@ -6,3 +6,32 @@ if(userEmail){
 else{
     console.log("Don't have user email")
 }
+
+// falsy values
+
+// false, 0, -0, bingint 0n, null, "", undefined, Nan,
+
+// truthy values
+
+// truth values,
+
+// "0", 'false' , ' ', [], {} , function(){} , 
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty")
+// }
+
+const empObj = {}
+
+if(Object.keys(empObj).length === 0){
+    console.log("Array is empty")
+}
+
+// Nullish coalescing operator(??)
+
+let val1;
+val1 = 5 ?? 10;
+
+
+console.log(val1);
+
