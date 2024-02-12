59e02304ae61ac21cd97c27d739584b008236252
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2024-02-12 10:45:00 +0000

    Terniary Operator

--- 04_ControlFlow/truthy.js
+++ 04_ControlFlow/truthy.js
@@ -30,8 +30,16 @@ if(Object.keys(empObj).length === 0){
 // Nullish coalescing operator(??)
 
 let val1;
-val1 = 5 ?? 10;
+// val1 = 5 ?? 10;
+//val1 = null ?? 10;
+// val1 = undefined ?? 15;
+val1 = null ?? 10 ?? 20;
 
 
 console.log(val1);
 
+//Terniary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("greater than 80") : console.log("less")
