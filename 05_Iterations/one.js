ebd4d8415e6bf5b805272a62ffad2e381053c324
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2024-02-14 10:34:19 +0000

    break and continue

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -7,8 +7,20 @@ for (let i = 0; i <= 10; i++) {
     }
     //console.log(i);
 }
+// nested for
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//     //console.log(`Inner loop value: ${j} and inner loop value: ${i}`);
+//     console.log( i + ' * ' + j + ' = ' + i*j)
+//     }
+// }
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    
+let myArray = ["hameed", "asif", "kashif"]
+ //console.log(myArray.length)
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+  //  console.log(element)
 }
+
+// break and continue
