1711eda6b96d0fb37f4fd12eedf21642b06b8aed
asifhameed8 <73539971+asifhameed8@users.noreply.github.com>
2024-02-16 10:54:05 +0000

    do while

--- 05_Iterations/two.js
+++ 05_Iterations/two.js
@@ -10,6 +10,13 @@ let myArray = ['Hameed', 'Kashif', 'Asif']
 
 let arr = 0;
 while (arr < myArray.length) {
-    console.log(`Value is ${myArray[arr]}`);
+  //  console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1;
 }
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
